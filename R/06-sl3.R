738fb11786b83ed4b7ee24daa75f59ad7a5e2d6c
rachaelvphillips <rachaelvphillips@gmail.com>
2021-02-22 10:52:45 -0800

    sl3 additions and make code + style

--- R/06-sl3.R
+++ R/06-sl3.R
@@ -1,16 +1,4 @@
-## ----cv_fig, echo = FALSE-----------------------------------------------------
-knitr::include_graphics("img/misc/vs.pdf")
-
-
-## ----cv_fig2, echo = FALSE----------------------------------------------------
-knitr::include_graphics("img/misc/SLKaiserNew.pdf")
-
-
-## ----cv_fig3, echo = FALSE----------------------------------------------------
-knitr::include_graphics("img/misc/ericSL.pdf")
-
-
-## ----setup, message=FALSE, warning=FALSE--------------------------------------
+## ----setup--------------------------------------------------------------------
 library(data.table)
 library(dplyr)
 library(readr)
@@ -22,15 +10,20 @@ library(knitr)
 library(kableExtra)
 
 # load data set and take a peek
-washb_data <- fread("https://raw.githubusercontent.com/tlverse/tlverse-data/master/wash-benefits/washb_data.csv",
-                    stringsAsFactors = TRUE)
+washb_data <- fread(
+  paste0(
+    "https://raw.githubusercontent.com/tlverse/tlverse-data/master/",
+    "wash-benefits/washb_data.csv"
+  ),
+  stringsAsFactors = TRUE
+)
 head(washb_data) %>%
   kable() %>%
   kableExtra:::kable_styling(fixed_thead = T) %>%
   scroll_box(width = "100%", height = "300px")
 
 
-## ----task---------------------------------------------------------------------
+## ----task, warning=TRUE-------------------------------------------------------
 # specify the outcome and covariates
 outcome <- "whz"
 covars <- colnames(washb_data)[-which(names(washb_data) == outcome)]
@@ -47,6 +40,15 @@ washb_task <- make_sl3_Task(
 washb_task
 
 
+## ----task-folds-examine-------------------------------------------------------
+length(washb_task$folds) # how many folds?
+
+head(washb_task$folds[[1]]$training_set) # row indexes for fold 1 training
+head(washb_task$folds[[1]]$validation_set) # row indexes for fold 1 validation
+
+any(washb_task$folds[[1]]$training_set %in% washb_task$folds[[1]]$validation_set)
+
+
 ## ----list-properties----------------------------------------------------------
 sl3_list_properties()
 
@@ -58,81 +60,123 @@ sl3_list_learners("continuous")
 ## ----baselearners-------------------------------------------------------------
 # choose base learners
 lrnr_glm <- make_learner(Lrnr_glm)
-lrnr_mean <- make_learner(Lrnr_mean)
+lrnr_mean <- Lrnr_mean$new()
 
 
-## ----extra-lrnr-awesome, message=FALSE, warning=FALSE-------------------------
+## ----extra-lrnr-awesome-------------------------------------------------------
 lrnr_ranger50 <- make_learner(Lrnr_ranger, num.trees = 50)
-lrnr_hal_simple <- make_learner(Lrnr_hal9001, max_degree = 2, n_folds = 2)
+lrnr_hal_faster <- Lrnr_hal9001$new(max_degree = 2, reduce_basis = 0.05)
 lrnr_lasso <- make_learner(Lrnr_glmnet) # alpha default is 1
-lrnr_ridge <- make_learner(Lrnr_glmnet, alpha = 0)
-lrnr_elasticnet <- make_learner(Lrnr_glmnet, alpha = .5)
+lrnr_ridge <- Lrnr_glmnet$new(alpha = 0)
+lrnr_elasticnet <- make_learner(Lrnr_glmnet, alpha = 0.5)
+xgb_fast <- Lrnr_xgboost$new()
 
 
-## ----extra-lrnr-woah, message=FALSE, warning=FALSE----------------------------
+## ----extra-lrnr-woah----------------------------------------------------------
 lrnr_bayesglm <- Lrnr_pkg_SuperLearner$new("SL.bayesglm")
 
 
 ## ----extra-lrnr-mindblown-svm, eval = FALSE-----------------------------------
 ## # I like to crock pot my super learners
-## grid_params <- list(cost = c(0.01, 0.1, 1, 10, 100, 1000),
-##                     gamma = c(0.001, 0.01, 0.1, 1),
-##                     kernel = c("polynomial", "radial", "sigmoid"),
-##                     degree = c(1, 2, 3))
+## grid_params <- list(
+##   cost = c(0.01, 0.1, 1, 10, 100, 1000),
+##   gamma = c(0.001, 0.01, 0.1, 1),
+##   kernel = c("polynomial", "radial", "sigmoid"),
+##   degree = c(1, 2, 3)
+## )
 ## grid <- expand.grid(grid_params, KEEP.OUT.ATTRS = FALSE)
-## params_default <- list(nthread = getOption("sl.cores.learners", 1))
-## svm_learners <- apply(grid, MARGIN = 1, function(params_tune) {
-##   do.call(Lrnr_svm$new, c(params_default, as.list(params_tune)))})
+## svm_learners <- apply(grid, MARGIN = 1, function(tuning_params) {
+##   do.call(Lrnr_svm$new, as.list(tuning_params))
+## })
 
 ## ----extra-lrnr-mindblown-xgboost---------------------------------------------
-grid_params <- list(max_depth = c(2, 4, 6, 8),
-                    eta = c(0.001, 0.01, 0.1, 0.2, 0.3),
-                    nrounds = c(20, 50))
+grid_params <- list(
+  max_depth = c(2, 4, 6),
+  eta = c(0.001, 0.1, 0.3),
+  nrounds = 100
+)
 grid <- expand.grid(grid_params, KEEP.OUT.ATTRS = FALSE)
-params_default <- list(nthread = getOption("sl.cores.learners", 1))
-xgb_learners <- apply(grid, MARGIN = 1, function(params_tune) {
-  do.call(Lrnr_xgboost$new, c(params_default, as.list(params_tune)))})
+xgb_learners <- apply(grid, MARGIN = 1, function(tuning_params) {
+  do.call(Lrnr_xgboost$new, as.list(tuning_params))
+})
 
 
 ## ----carotene, eval = FALSE---------------------------------------------------
-## # I have no idea how to tune a neural net (or BART machine..)
+## # Unlike xgboost, I have no idea how to tune a neural net or BART machine, so
+## # I let caret take the reins
 ## lrnr_caret_nnet <- make_learner(Lrnr_caret, algorithm = "nnet")
-## lrnr_caret_bartMachine <- make_learner(Lrnr_caret, algorithm = "bartMachine",
-##                                        method = "boot", metric = "Accuracy",
-##                                        tuneLength = 10)
+## lrnr_caret_bartMachine <- make_learner(Lrnr_caret,
+##   algorithm = "bartMachine",
+##   method = "boot", metric = "Accuracy",
+##   tuneLength = 10
+## )
 
 
 ## ----stack--------------------------------------------------------------------
-stack <- make_learner(
-  Stack,
-  lrnr_glm, lrnr_mean, lrnr_ridge, lrnr_lasso, xgb_learners[[10]]
-)
+stack <- make_learner(Stack, lrnr_glm, lrnr_ridge, lrnr_lasso, xgb_fast)
+stack
+
+
+## ----alt-stack----------------------------------------------------------------
+# named vector of learners first
+learners <- c(lrnr_glm, lrnr_ridge, lrnr_lasso, xgb_fast)
+names(learners) <- c("glm", "ridge", "lasso", "xgboost")
+# next make the stack
+stack <- make_learner(Stack, learners)
+# now the names are pretty
+stack
+
+
+## ----alt-stack-cv-------------------------------------------------------------
+cv_stack <- Lrnr_cv$new(stack)
+cv_stack
 
 
 ## ----screener-----------------------------------------------------------------
-screen_rf <- make_learner(Lrnr_screener_randomForest, nVar = 5, ntree = 20)
+miniforest <- Lrnr_ranger$new(
+  num.trees = 20, write.forest = FALSE,
+  importance = "impurity_corrected"
+)
+
+# learner must already be instantiated, we did this when we created miniforest
+screen_rf <- Lrnr_screener_importance$new(learner = miniforest, num_screen = 5)
+screen_rf
+
 # which covariates are selected on the full data?
 screen_rf$train(washb_task)
+
+
+## ----screener-augment---------------------------------------------------------
+keepme <- c("aged", "momage")
+# screener must already be instantiated, we did this when we created screen_rf
+screen_augment_rf <- Lrnr_screener_augment$new(
+  screener = screen_rf, default_covariates = keepme
+)
+screen_augment_rf
 
 
 ## ----screener-pipe------------------------------------------------------------
 screen_rf_pipeline <- make_learner(Pipeline, screen_rf, stack)
 
 
-## ----screeners-stack, message=FALSE, warning=FALSE----------------------------
-fancy_stack <- make_learner(Stack, screen_rf_pipeline, stack)
+## ----screeners-stack----------------------------------------------------------
+# pretty names again
+learners2 <- c(learners, screen_rf_pipeline)
+names(learners2) <- c(names(learners), "rf_screen")
+
+fancy_stack <- make_learner(Stack, learners2)
+fancy_stack
+
 # we can visualize the stack
 dt_stack <- delayed_learner_train(fancy_stack, washb_task)
 plot(dt_stack, color = FALSE, height = "400px", width = "90%")
 
 
-## ----make-sl, message=FALSE, warning=FALSE------------------------------------
-sl <- make_learner(Lrnr_sl,
-  learners = fancy_stack
-)
+## ----make-sl------------------------------------------------------------------
+sl <- make_learner(Lrnr_sl, learners = fancy_stack)
 
 
-## ----make-sl-plot, message=FALSE, warning=FALSE-------------------------------
+## ----make-sl-plot-------------------------------------------------------------
 dt_sl <- delayed_learner_train(sl, washb_task)
 plot(dt_sl, color = FALSE, height = "400px", width = "90%")
 
@@ -149,15 +193,19 @@ head(sl_preds)
 
 ## ---- plot-predvobs-woohoo, eval=FALSE----------------------------------------
 ## 
-## df_plot <- data.frame(Observed = washb_data$whz,
-##                       Predicted = sl_preds,
-##                       count = c(1:nrow(washb_data)))
+## # df_plot <- data.frame(
+##   Observed = washb_data$whz,
+##   Predicted = sl_preds,
+##   count = c(1:nrow(washb_data))
+## )
 ## 
-## df_plot_melted <- melt(df_plot,
-##                        id.vars = "count",
-##                        measure.vars = c("Observed", "Predicted"))
+## # df_plot_melted <- melt(df_plot,
+##   id.vars = "count",
+##   measure.vars = c("Observed", "Predicted")
+## )
 ## 
-## ggplot(df_plot_melted, aes(value, count, color = variable)) + geom_point()
+## # ggplot(df_plot_melted, aes(value, count, color = variable)) +
+##   geom_point()
 
 
 ## ---- sl-summary--------------------------------------------------------------
@@ -171,7 +219,9 @@ washb_task_new <- make_sl3_Task(
   outcome = outcome,
   folds = origami::make_folds(washb_data, fold_fun = folds_vfold, V = 2)
 )
-CVsl <- CV_lrnr_sl(sl_fit, washb_task_new, loss_squared_error)
+CVsl <- CV_lrnr_sl(
+  lrnr_sl = sl_fit, task = washb_task_new, loss_fun = loss_squared_error
+)
 CVsl %>%
   kable(digits = 4) %>%
   kableExtra:::kable_styling(fixed_thead = T) %>%
@@ -179,38 +229,43 @@ CVsl %>%
 
 
 ## ----varimp-------------------------------------------------------------------
-washb_varimp <- varimp(sl_fit, loss_squared_error)
+washb_varimp <- importance(sl_fit, loss = loss_squared_error, type = "permute")
 washb_varimp %>%
   kable(digits = 4) %>%
-  kableExtra:::kable_styling(fixed_thead = T) %>%
+  kableExtra:::kable_styling(fixed_thead = TRUE) %>%
   scroll_box(width = "100%", height = "300px")
 
 
-## ----varimp-plot, warning = FALSE, message = FALSE----------------------------
+## ----varimp-plot--------------------------------------------------------------
 # plot variable importance
-washb_varimp %>%
-  mutate(name = forcats::fct_reorder(X, risk_diff)) %>%
-  ggplot(aes(x = risk_diff, y = name)) +
-  geom_dotplot(binaxis = "y") +
-  labs(x = "Risk Difference", y = "Covariate", 
-       title = "sl3 Variable Importance for WASH Benefits Example Data")
+importance_plot(
+  washb_varimp,
+  main = "sl3 Variable Importance for WASH Benefits Example Data"
+)
 
 
-## ----ex-setup, warning=FALSE, message=FALSE-----------------------------------
+## ----ex-setup-----------------------------------------------------------------
 # load the data set
-db_data <-
- url("https://raw.githubusercontent.com/benkeser/sllecture/master/chspred.csv")
+db_data <- url(
+  paste0(
+    "https://raw.githubusercontent.com/benkeser/sllecture/master/",
+    "chspred.csv"
+  )
+)
 chspred <- read_csv(file = db_data, col_names = TRUE)
+
 # take a quick peek
 head(chspred) %>%
   kable(digits = 4) %>%
-  kableExtra:::kable_styling(fixed_thead = T) %>%
+  kableExtra:::kable_styling(fixed_thead = TRUE) %>%
   scroll_box(width = "100%", height = "300px")
 
 
-## ----ex-setup2, warning=FALSE, message=FALSE----------------------------------
-ist_data <- paste0("https://raw.githubusercontent.com/tlverse/",
-                   "tlverse-handbook/master/data/ist_sample.csv") %>% fread()
+## ----ex-setup2----------------------------------------------------------------
+ist_data <- paste0(
+  "https://raw.githubusercontent.com/tlverse/",
+  "tlverse-handbook/master/data/ist_sample.csv"
+) %>% fread()
 
 # number 3 help
 ist_task_CVsl <- make_sl3_Task(
@@ -222,13 +277,14 @@ ist_task_CVsl <- make_sl3_Task(
     n = sum(!is.na(ist_data$DRSISC)),
     fold_fun = folds_vfold,
     V = 5
-    )
   )
+)
 
 
-## ----ex-key, eval=FALSE, message=FALSE, warning=FALSE-------------------------
-## db_data <-
-##  url("https://raw.githubusercontent.com/benkeser/sllecture/master/chspred.csv")
+## ----ex-key, eval=FALSE-------------------------------------------------------
+## db_data <- url(
+##   "https://raw.githubusercontent.com/benkeser/sllecture/master/chspred.csv"
+## )
 ## chspred <- read_csv(file = db_data, col_names = TRUE)
 ## 
 ## # make task
@@ -236,7 +292,7 @@ ist_task_CVsl <- make_sl3_Task(
 ##   data = chspred,
 ##   covariates = head(colnames(chspred), -1),
 ##   outcome = "mi"
-##   )
+## )
 ## 
 ## # make learners
 ## glm_learner <- Lrnr_glm$new()
@@ -262,33 +318,32 @@ ist_task_CVsl <- make_sl3_Task(
 ##   lasso_learner, ridge_learner, enet_learner,
 ##   curated_glm_learner, mean_learner, glm_fast_learner,
 ##   ranger_learner, svm_learner, xgb_learner
-##   )
+## )
 ## 
 ## # make and train super learner
 ## sl <- Lrnr_sl$new(
 ##   learners = stack
-##   )
+## )
 ## sl_fit <- sl$train(chspred_task)
 ## sl_fit$print()
 ## 
 ## CVsl <- CV_lrnr_sl(sl_fit, chspred_task, loss_squared_error)
 ## CVsl
 ## 
-## importance <- varimp(sl_fit, loss_squared_error)
-## importance %>%
-##   mutate(name = forcats::fct_reorder(X, risk_diff)) %>%
-##   ggplot(aes(x = risk_diff, y = name)) +
-##   geom_dotplot(binaxis = "y") +
-##   labs(x = "Risk Difference", y = "Covariate",
-##        title = "sl3 Variable Importance for Myocardian Infarction Prediction")
-## 
+## varimp <- importance(sl_fit, loss_squared_error, type = "permute")
+## varimp %>%
+##   importance_plot(
+##     main = "sl3 Variable Importance for Myocardial Infarction Prediction"
+##   )
 
 
 ## ----ex2-key, eval=FALSE------------------------------------------------------
 ## library(ROCR) # for AUC calculation
 ## 
-## ist_data <- paste0("https://raw.githubusercontent.com/tlverse/",
-##                    "tlverse-handbook/master/data/ist_sample.csv") %>% fread()
+## ist_data <- paste0(
+##   "https://raw.githubusercontent.com/tlverse/",
+##   "tlverse-handbook/master/data/ist_sample.csv"
+## ) %>% fread()
 ## 
 ## # stack
 ## ist_task <- make_sl3_Task(
@@ -296,7 +351,7 @@ ist_task_CVsl <- make_sl3_Task(
 ##   outcome = "DRSISC",
 ##   covariates = colnames(ist_data)[-which(names(ist_data) == "DRSISC")],
 ##   drop_missing_outcome = TRUE
-##   )
+## )
 ## 
 ## # learner library
 ## lrnr_glm <- Lrnr_glm$new()
@@ -307,15 +362,21 @@ ist_task_CVsl <- make_sl3_Task(
 ## lrnr_ranger <- Lrnr_ranger$new()
 ## lrnr_svm <- Lrnr_svm$new()
 ## # xgboost grid
-## grid_params <- list(max_depth = c(2, 5, 8),
-##                     eta = c(0.01, 0.15, 0.3))
+## grid_params <- list(
+##   max_depth = c(2, 5, 8),
+##   eta = c(0.01, 0.15, 0.3)
+## )
 ## grid <- expand.grid(grid_params, KEEP.OUT.ATTRS = FALSE)
 ## params_default <- list(nthread = getOption("sl.cores.learners", 1))
 ## xgb_learners <- apply(grid, MARGIN = 1, function(params_tune) {
-##   do.call(Lrnr_xgboost$new, c(params_default, as.list(params_tune)))})
-## learners <- unlist(list(xgb_learners, lrnr_ridge, lrnr_mean, lrnr_lasso,
-##                         lrnr_glm, lrnr_enet, lrnr_ranger, lrnr_svm),
-##                    recursive = TRUE)
+##   do.call(Lrnr_xgboost$new, c(params_default, as.list(params_tune)))
+## })
+## learners <- unlist(list(
+##   xgb_learners, lrnr_ridge, lrnr_mean, lrnr_lasso,
+##   lrnr_glm, lrnr_enet, lrnr_ranger, lrnr_svm
+## ),
+## recursive = TRUE
+## )
 ## 
 ## # super learner
 ## sl <- Lrnr_sl$new(learners)
@@ -337,20 +398,17 @@ ist_task_CVsl <- make_sl3_Task(
 ##     n = sum(!is.na(ist_data$DRSISC)),
 ##     fold_fun = folds_vfold,
 ##     V = 5
-##     )
 ##   )
+## )
 ## CVsl <- CV_lrnr_sl(sl_fit, ist_task_CVsl, loss_loglik_binomial)
 ## CVsl
 ## 
 ## # sl3 variable importance plot
-## importance <- varimp(sl_fit, loss_loglik_binomial)
-## 
-## importance %>%
-##   mutate(name = forcats::fct_reorder(X, risk_diff)) %>%
-##   ggplot(aes(x = risk_diff, y = name)) +
-##   geom_dotplot(binaxis = "y") +
-##   labs(x = "Risk Difference", y = "Covariate",
-##        title = "Variable Importance for Predicting Recurrent Ischemic Stroke")
+## ist_varimp <- importance(sl_fit, loss_loglik_binomial, type = "permute")
+## ist_varimp %>%
+##   importance_plot(
+##     main = "Variable Importance for Predicting Recurrent Ischemic Stroke"
+##   )
 
 
 ## ----ex3-key, eval=FALSE------------------------------------------------------
