e0eb2c4b515fb5c11feb700a23e8a53223e3cf81
Nima Hejazi <nh@nimahejazi.org>
2021-04-18 15:05:01 -0700

    fix knit_hooks

--- _common.R
+++ _common.R
@@ -2,14 +2,14 @@
 library(methods)
 set.seed(34729)
 
-hook_output = knit_hooks$get('output')
-knit_hooks$set(output = function(x, options) {
+hook_output <- knit_hooks$get("output")
+knitr::knit_hooks$set(output = function(x, options) {
   # this hook is used only when the linewidth option is not NULL
   if (!is.null(n <- options$linewidth)) {
-    x = knitr:::split_lines(x)
+    x <- knitr:::split_lines(x)
     # any lines wider than n should be wrapped
-    if (any(nchar(x) > n)) x = strwrap(x, width = n)
-    x = paste(x, collapse = '\n')
+    if (any(nchar(x) > n)) x <- strwrap(x, width = n)
+    x <- paste(x, collapse = "\n")
   }
   hook_output(x, options)
 })
