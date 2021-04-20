79f1a88e8616f1d8cb056e104674100b738bb30a
Nima Hejazi <nh@nimahejazi.org>
2021-04-20 15:46:25 -0700

    add output format build matrix for Travis

--- _build.sh
+++ _build.sh
@@ -1,5 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+echo "Building bookdown $BOOKDOWN_FORMAT"
+case "$BOOKDOWN_FORMAT" in
+   PDF) BOOKDOWN_OUTPUT="pdf_book" ;;
+  SITE) BOOKDOWN_OUTPUT="bookdown_site" ;;
+esac
 
 Rscript -e "bookdown::clean_book(TRUE)"
-Rscript -e "bookdown::render_book('index.Rmd', quiet = FALSE)"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::$(BOOKDOWN_OUTPUT)')"
