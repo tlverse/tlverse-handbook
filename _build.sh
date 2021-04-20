3bd930ba77c2b23edc8ad4736d09eda20d9f6b02
Nima Hejazi <nh@nimahejazi.org>
2021-04-20 16:30:41 -0700

    change env var handline in build script

--- _build.sh
+++ _build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo "Building bookdown $BOOKDOWN_FORMAT"
-case "$BOOKDOWN_FORMAT" in
-   PDF) BOOKDOWN_OUTPUT="pdf_book" ;;
-  SITE) BOOKDOWN_OUTPUT="bookdown_site" ;;
-esac
+if [ "$BOOKDOWN_FORMAT" == "SITE" ]
+then
+    BOOKDOWN_OUTPUT=bookdown_site
+else
+    BOOKDOWN_OUTPUT=pdf_book
+fi
 
 Rscript -e "bookdown::clean_book(TRUE)"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::$(BOOKDOWN_OUTPUT)')"
